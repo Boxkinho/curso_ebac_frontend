9c0c777095bd04455e82432f7c3c96fe5ac79bd8
boxkinho <jbmmf@yahoo.com.br>
2024-02-21 21:32:27 -0300

    exercicio ajax

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+
+    fetch('https://api.github.com/users/Boxkinho')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(json) {
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url;
+            followersElement.innerText = json.followers;
+            followingElement.innerText = json.following;
+            repos.innerText = json.public_repos;
+        })
+})
