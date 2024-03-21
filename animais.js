7b5c7a19057cfdf68e4fcdf4b0c0d2052c9299aa
boxkinho <jbmmf@yahoo.com.br>
2024-03-20 22:09:44 -0300

    Orientado a Objeto JS alterado

--- animais.js
+++ animais.js
@@ -1,23 +1,30 @@
-function Animal (nome) {
-    this.nome = nome;
-}
-
-function Cachorro (nome, idade, raca) {
-    this.nome = nome;
-    this.idade = idade;
-    this.raca = raca;
-}
-
-function Gato (nome, idade, pelo) {
+function Animal (nome, idade, pelo, raca) {
     this.nome = nome;
     this.idade = idade;
     this.pelo = pelo;
+    this.raca = raca;
 }
 
-const cachorro1 = new Cachorro("Thor", 7, "Lavrador");
-const gato1 = new Gato("Garfildes", 5, "branco");
-const cachorro2 = new Cachorro("Bobby", 4, "Pastor Alemão");
+function Cachorro (nome, idade, pelo, raca) {
+    Animal.call(this, nome, idade, pelo, raca);
 
-console.log("Nome: " + cachorro1.nome + " | Idade: " + cachorro1.idade + " | Raça: "+ cachorro1.raca);
-console.log("Nome: " + gato1.nome + " | Idade: " + gato1.idade + " | Cor do pelo: "+ gato1.pelo);
-console.log("Nome: " + cachorro2.nome + " | Idade: " + cachorro2.idade + " | Raça: "+ cachorro2.raca);
+    this.latir = function() {
+        return 'Au Au';
+    }
+}
+
+function Gato (nome, idade, pelo, raca) {
+    Animal.call(this, nome, idade, pelo, raca);
+
+    this.miar = function() {
+        return 'Miau';
+    }
+}
+
+const cachorro1 = new Cachorro("Thor", 7, "preto", "Lavrador");
+const gato1 = new Gato("Garfildes", 5, "marrom", "Perça");
+const cachorro2 = new Cachorro("Bobby", 4, "preto", "Pastor Alemão");
+
+console.log(cachorro1.latir() + " | Nome: " + cachorro1.nome + " | Idade: " + cachorro1.idade + " | Cor do pelo: " + cachorro1.pelo + " | Raça: " + cachorro1.raca);
+console.log(gato1.miar() + " | Nome: " + gato1.nome + " | Idade: " + gato1.idade + " | Cor do pelo: " + gato1.pelo + " | Raça: "+ gato1.raca);
+console.log(cachorro2.latir() + " | Nome: " + cachorro2.nome + " | Idade: " + cachorro2.idade + " | Cor do pelo: " + cachorro2.pelo + " | Raça: "+ cachorro2.raca);
